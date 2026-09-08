b0ea90440f3f57a0a32e4bf2f24d71816e83e6f3
EspinalCarlos <cespinalb@gmail.com>
2026-09-07 22:14:26 -0600

    Ultimos tres automatas creados

--- Proyecto1/Automatas.hpp
+++ Proyecto1/Automatas.hpp
@@ -6,5 +6,7 @@
 bool esIdentificador(const std::string& cadena);
 bool esNumero(const std::string& cadena);
 bool esString(const std::string& cadena);
-
+bool esChar(const std::string& cadena);
+bool esOperador(const std::string& cadena);
+bool esDelimitador(const std::string& cadena);
 #endif
